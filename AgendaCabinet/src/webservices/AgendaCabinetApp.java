b7668643deefbdc09853b22f716ffa1cbb87dcb0
andora2 <andora@baetu.de>
2017-04-02 18:54:34 +0200

    remove subfolder

--- AgendaCabinet/src/webservices/AgendaCabinetApp.java
+++
@@ -1,9 +0,0 @@
-package webservices;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("rest")
-public class AgendaCabinetApp extends Application {
-
-}
